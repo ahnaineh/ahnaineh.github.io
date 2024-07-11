51c67d923fcc8e221757bb5b527483c490d9394e
Ali Hnaineh <ahhnainehh@gmail.com>
2024-07-12 02:14:14 +0300

    v3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2239063515"
+    serviceWorkerVersion: "1362053424"
   }
 });
